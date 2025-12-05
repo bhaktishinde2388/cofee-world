a3cf37305feac26e4076bb561fcf80b9eec620ec
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2025-12-06 01:42:54 +0530

    feat: create the array for images and  integrate Carousel component into Home page where i pass the 'images' and 'intervel' of img as props

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar.jsx'
 import Carousel from '../../components/Carousel/Carousel.jsx'
+import Coffee1 from '../../assets/images/Coffee-1.jpg'
+import Coffee2 from '../../assets/images/Coffee-2.webp'
+import Coffee3 from '../../assets/images/Coffee-3.jpg'
+import Coffee4 from '../../assets/images/Coffee-4.webp'
+
 import "./Home.css"
 
 function Home() {
+
+  const carouselImages = [Coffee1,Coffee2,Coffee3,Coffee4];
+
   return (
    <>
    <Navbar />
-   <Carousel/>
+   <Carousel images={carouselImages} interval={3000}/>
    Home
    </>
   )
