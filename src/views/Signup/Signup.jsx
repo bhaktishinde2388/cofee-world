49ccfcdbddec01d1581296daee3d24eb8c04862b
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2025-12-06 02:21:18 +0530

    feat: create simple design of signup page

--- src/views/Signup/Signup.jsx
+++ src/views/Signup/Signup.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import './Signup.css'
 
 function Signup() {
   return (
-    <div>Signup</div>
+       <div className='forms-container'>
+      <input className='usename-input input-box' type="text" placeholder='Enter UserName' />
+       <input className='password-input input-box' type="text" placeholder='Enter UserName' />
+        <input className='confirm-pasword-input input-box' type="text" placeholder='Enter UserName' />
+    </div>
   )
 }
 
