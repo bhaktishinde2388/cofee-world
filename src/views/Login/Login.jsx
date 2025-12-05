377f46730f5d05910d6a0fadb4bea13c56e521ef
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2025-12-06 02:56:00 +0530

    feat : login page created

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Button from '../../components/Button/Button'
+import { Link } from 'react-router-dom'
+
 
 function Login() {
   return (
-    <div>Login</div>
+     <div className='signup-bg'>
+       <form className='forms-container'>
+      <input className='usename-input input-box' type="text" placeholder='Enter UserName' />
+       <input className='password-input input-box' type="text" placeholder='Enter Password' />
+        <Button text='login'/>
+
+           <p className="login-text">
+         Don't have an account? <Link to="/signup">Signup</Link>
+        </p>
+    </form>
+    </div>
   )
 }
 
