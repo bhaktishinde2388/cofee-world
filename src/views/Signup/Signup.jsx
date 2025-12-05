db7f54d03b2d48293181e9e78f478ad4cc5af45b
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2025-12-06 02:47:10 +0530

    feat : modified css for signup page where the bg image is added and applied some other styles

--- src/views/Signup/Signup.jsx
+++ src/views/Signup/Signup.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import Button from "../../components/Button/Button.jsx"
 import './Signup.css'
 
 function Signup() {
   return (
-       <div className='forms-container'>
+    <div className='signup-bg'>
+       <form className='forms-container'>
       <input className='usename-input input-box' type="text" placeholder='Enter UserName' />
-       <input className='password-input input-box' type="text" placeholder='Enter UserName' />
-        <input className='confirm-pasword-input input-box' type="text" placeholder='Enter UserName' />
+       <input className='password-input input-box' type="text" placeholder='Enter Password' />
+        <input className='confirm-pasword-input input-box' type="text" placeholder='Confirm Password' />
+        <Button text='signup'/>
+
+        
+    </form>
     </div>
   )
 }
